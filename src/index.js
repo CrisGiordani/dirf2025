66acc68ad1df935e0fafa9a837cace895ad0283d
Cristian Giordani <cristiangiordani@gmail.com>
2025-02-26 13:44:30 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-const express = require('express')
+import express from 'express'
+import { getDados } from './generate'
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/generate/:cpf', getDados)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
